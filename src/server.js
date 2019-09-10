e0004ff591da69b4f8ad3072d707bee348e2f8e1
Sammy Nave <sammy@mentorcollective.org>
2019-09-10 09:12:19 -0400

    some cleanup

--- src/server.js
+++ src/server.js
@@ -1,20 +1,17 @@
 import sirv from 'sirv';
-import express from 'express';
-import http from 'http';
+import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const app = express() // You can also use Express
+const server = polka() // You can also use Express
   .use(
     compression({ threshold: 0 }),
     sirv('static', { dev }),
     sapper.middleware()
   );
-
-const server = http.createServer(app);
 
 server.listen(PORT, err => {
   if (err) console.log('error', err);
