32d028ab830b0ccaa5c150380be9e599cf8c8e64
Sammy Nave <sammy@mentorcollective.org>
2019-10-19 22:33:30 -0400

    add caching

--- src/api/graphql.js
+++ src/api/graphql.js
@@ -1,7 +1,14 @@
 import { API_URL } from '../constants';
 import { throwError } from './error-handler';
 
+const cache = {};
+
 export const query = async ({ body, fetch }) => {
+  const cached = cache[body];
+  if (cached) {
+    return cached;
+  }
+
   const response = await fetch(`${API_URL}/graphql.json`, {
     method: 'POST',
     mode: 'cors',
@@ -15,12 +22,13 @@ export const query = async ({ body, fetch }) => {
   const responseBody = await response.json();
 
   if (!response.ok) {
+    delete cache[body];
     return throwError({
       status: response.status,
       text: response.statusText,
       errors: [responseBody.error]
     });
   }
-
+  cache[body] = responseBody;
   return responseBody;
 }
